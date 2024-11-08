b19001ed8d81a6f69e0eeac0685969ed1be9c7d9
junghojang <cookyman@gmail.com>
2024-11-08 20:00:06 +0900

    ready to develop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>My Music App</h1>
+        <p>React 프로젝트가 준비되었습니다!</p>
+      </div>
   );
 }
 
